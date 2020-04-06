59b574b65092a167af4f3323883da00fdb325e42
Hanna Söderström <hanna.soderstrom@netigate.se>
2020-04-06 17:22:43 +0200

    feat: add svg-icons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,15 +9,90 @@ enum ButtonType {
   play = 'play',
 }
 
+const PlayIcon = (): JSX.Element => (
+  <svg
+    fill="none"
+    height="42"
+    viewBox="0 0 42 42"
+    width="42"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="21" cy="21" fill="#C4C4C4" r="21" />
+    <path d="M38 20.5L11.75 35.6554L11.75 5.34456L38 20.5Z" fill="white" />
+  </svg>
+)
+
+const PauseIcon = (): JSX.Element => (
+  <svg
+    fill="none"
+    height="42"
+    viewBox="0 0 42 42"
+    width="42"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="21" cy="21" fill="#C4C4C4" r="21" />
+    <rect fill="white" height="29" width="7" x="10" y="7" />
+    <rect fill="white" height="29" width="7" x="25" y="7" />
+  </svg>
+)
+
+const NextIcon = (): JSX.Element => (
+  <svg
+    fill="none"
+    height="42"
+    viewBox="0 0 42 42"
+    width="42"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="21" cy="21" fill="#C4C4C4" r="21" />
+    <path d="M39 20.5L22.5 33.0574L22.5 7.94263L39 20.5Z" fill="white" />
+    <path d="M27 20.5L10.5 33.0574L10.5 7.94263L27 20.5Z" fill="white" />
+  </svg>
+)
+
+const PreviousIcon = (): JSX.Element => (
+  <svg
+    fill="none"
+    height="42"
+    viewBox="0 0 42 42"
+    width="42"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="21"
+      cy="21"
+      fill="#C4C4C4"
+      r="21"
+      transform="rotate(-180 21 21)"
+    />
+    <path d="M3 21.5L19.5 8.94263L19.5 34.0574L3 21.5Z" fill="white" />
+    <path d="M15 21.5L31.5 8.94263L31.5 34.0574L15 21.5Z" fill="white" />
+  </svg>
+)
+
+type IconType = {
+  type: keyof typeof ButtonType
+}
+
+function IconType({ type }): JSX.Element {
+  const icons = {
+    play: <PlayIcon />,
+    left: <PreviousIcon />,
+    right: <NextIcon />,
+    pause: <PauseIcon />,
+  }
+  return icons[type]
+}
+
 type ButtonProps = {
-  onClick?: any;
-  type: keyof typeof ButtonType;
+  onClick?: any
+  type: keyof typeof ButtonType
 }
 
 function Button({ onClick, type }: ButtonProps): JSX.Element {
   return (
     <button className="button" onClick={onClick}>
-      <span className={type}></span>
+      <IconType type={type} />
     </button>
   )
 }
